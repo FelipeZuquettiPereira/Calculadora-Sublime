3c08004b138bc933611bcd07ca16d6268e7b227d
FelipeZuquetti <felipe_z_pereira@estudante.sesisenai.org.br>
2026-03-29 21:51:43 -0300

    Botões Funcionando

--- Javas/Calculadora1.js
+++ Javas/Calculadora1.js
@@ -1,3 +1,13 @@
-function Numero1(){
-    
+let tela = document.getElementById("tela");
+let currentInput = ""
+let currentOperator = ""
+
+function Numero1(value){
+    currentInput += value
+    tela.textContent = currentInput
+
+}
+
+function reset(value){
+    value = "  "
 }
